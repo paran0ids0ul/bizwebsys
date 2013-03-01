bf8b36632568e07723461dd0f6c7429fd38ca85d
Horace Li <horaceli@gmail.com>
2013-03-01 06:35:49 +0000

    gitignore updated

--- application/logs/log-2013-02-22.php
+++ application/logs/log-2013-02-22.php
@@ -4,3 +4,4 @@ ERROR - 2013-02-22 01:42:47 --> 404 Page Not Found --> resources
 ERROR - 2013-02-22 01:42:53 --> 404 Page Not Found --> resources
 ERROR - 2013-02-22 01:51:53 --> 404 Page Not Found --> resources
 ERROR - 2013-02-22 01:55:29 --> 404 Page Not Found --> resources
+ERROR - 2013-02-22 14:53:52 --> 404 Page Not Found --> resources
