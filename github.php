a873243facb1e31e2c21b336ed4566e06294f72c
Horace Li <horaceli@gmail.com>
2013-03-01 10:28:38 +0000

    Update github.php

--- github.php
+++ github.php
@@ -3,6 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 set_time_limit(0);
  
-    $output = shell_exec("/var/www/BusinessWebSys/git-puller.sh");
+    $output = shell_exec("/var/www/bizwebsys/git-puller.sh");
  
 ?>
