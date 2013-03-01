1d0b5f045f66a10502e4b76f839343ce003dc593
Horace Li <horaceli@gmail.com>
2013-03-01 06:37:37 +0000

    rm log

--- application/logs/log-2013-02-22.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-02-22 01:42:47 --> 404 Page Not Found --> resources
-ERROR - 2013-02-22 01:42:53 --> 404 Page Not Found --> resources
-ERROR - 2013-02-22 01:51:53 --> 404 Page Not Found --> resources
-ERROR - 2013-02-22 01:55:29 --> 404 Page Not Found --> resources
-ERROR - 2013-02-22 14:53:52 --> 404 Page Not Found --> resources
