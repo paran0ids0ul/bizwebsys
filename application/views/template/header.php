a8f7f09580869bcd121698772be29975ee6b4bbb
Ee Ren <zcesh69@ucl.ac.uk>
2013-03-05 16:37:48 +0000

    modifying header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,15 +1,37 @@
 <header id="header" class="navbar navbar-fixed-top">
-  <div class="navbar-inner">
-    <div class="container">
-		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-       </a>
-	   <div class="nav-collapse collapse" id="main-menu">
-		   <?php echo $nav?>
-		</div>   
-		   <?php echo $user?>
-    </div>
-  </div>
+  <div class="row-fluid">
+    <div class="span12">
+      <div class="navbar">
+        <div class="navbar-inner">
+          <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+            <div class="btn-group pull-right">
+              <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="icon-user"></i> Username
+                <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Profile</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Sign Out</a></li>
+              </ul>
+            </div>
+            <div class="nav-collapse">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div><!-- end navbar -->
+    </div><!-- end span8 -->
+  </div><!-- end row -->
 </header>
+
+
