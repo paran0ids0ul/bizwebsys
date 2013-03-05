7d7bf90a6439f58ec4ac0fb8fff5dfb8359369b2
Ee Ren <zcesh69@ucl.ac.uk>
2013-03-05 18:22:50 +0000

    Fix home_panel

--- application/views/panel/home_panel.php
+++ application/views/panel/home_panel.php
@@ -3,27 +3,27 @@
     <div class="span2">
       <ul class="nav nav-list">
 			<li class="nav-header">Bulletin Board</li>
-			<li class="active"><a href="#">Whole Company</a></li>
-			<li><a href="#">Group 1</a></li>
-			<li><a href="#">Group 2</a></li>
-			<li><a href="#">Add Group</a></li>
+			<li class="active sub-header"><a href="#">Whole Company</a></li>
+			<li class="sub-header"><a href="#">phpCode in here</a></li>    <!-- dynamic to get groups-->
+			<li class="sub-header"><a href="#">Add Group</a></li>
 			<li class="nav-header">Email</li>
-			<li><a href="#">Inbox</a></li>
-			<li><a href="#">Todo</a></li> 
+			<li class="sub-header"><a href="#">Inbox</a></li>
+			<li	class="sub-header"><a href="#">Todo</a></li> 
 			<li class="nav-header">Calendar</li>
-			<li><a href="#">Work</a></li>  
+			<li	class="sub-header"><a href="#">Work</a></li>  
 		</ul>
     </div>
     <div class="span10">
+    
       	<div class="row">
 		<!-- Bulletin Board -->
-			<h5>Bulletin Board:Whole Company</h5>             <!--  TODO:dynamic get group name -->
-			<pre class = "pre-scrollable">Sample text here...</pre>
+			<p class="nav-header content-header">Bulletin Board:Whole Company</p>             <!--  TODO:dynamic get group name -->
+			<pre class = "pre-scrollable" id="bulletin-board">Sample text here...</pre>
 		</div>	
 		<div class="row">
 			<h5>Post Notice</h5> 
-			<textarea class="span9" rows="4"></textarea>
-		    <button class="btn btn-primary pull-right" type="button">Post</button>
+			<textarea class="span9"></textarea>
+		    <button class="btn btn-primary" type="button">Post</button>
 		</div>
     </div>
   </div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,37 +1,42 @@
 <header id="header" class="navbar navbar-fixed-top">
-  <div class="row-fluid">
-    <div class="span12">
-      <div class="navbar">
-        <div class="navbar-inner">
-          <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </a>
-            <div class="btn-group pull-right">
-              <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="icon-user"></i> Username
-                <span class="caret"></span>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Profile</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Sign Out</a></li>
-              </ul>
-            </div>
-            <div class="nav-collapse">
-              <ul class="nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div><!-- end navbar -->
-    </div><!-- end span8 -->
-  </div><!-- end row -->
+	<div class="navbar .navbar-static-top">
+		<div class="navbar-inner">
+			<div class="container">
+    
+    
+    
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				
+				
+				<a class="brand" href="#">BizWebSys</a>
+				
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">Workspace</a></li>
+						<li><a href="#">Settings</a></li>
+					</ul>
+					<form class="navbar-form pull-right" id="user-button">
+						<?php echo $user?>
+					</form>
+        		</div>
+				
+				
+    
+				
+				
+				
+				
+			</div>
+		</div>
+  	</div>
 </header>
+
+ 
+ 
 
 
