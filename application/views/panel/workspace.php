4a2a2175bc700d91c55c640188e3556fb96cb5e7
Liang <vianessa@163.com>
2013-03-06 16:25:44 +0000

    workspace

--- application/views/panel/workspace.php
+++ application/views/panel/workspace.php
@@ -5,26 +5,26 @@
 <ul class="thumbnails">
   <li class="span2">
     <a href="<?php echo site_url("inventory");?>" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/icons/inventory.png")?> alt="Inventory">
+      <img src=<?php echo base_url("resources/images/icons/inventory.png")?> alt="Inventory" width="90" height="90">
 	  <h4><center>Inventory</center></h4>
 	</a>
   </li>
   <li class="span2">
     <a href="<?php echo site_url("contacts");?>" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/icons/contact.png")?> alt="Contacts">
+      <img src=<?php echo base_url("resources/images/icons/contact.png")?> alt="Contacts"  width="90" height="90">
     <h4><center>Contacts</center></h4>
 	</a>
   </li>
   <li class="span2">
     <a href="<?php echo site_url("pos");?>" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/icons/POS.png")?> alt="POS">
+      <img src=<?php echo base_url("resources/images/icons/pos.png")?> alt="POS"  width="90" height="90">
 	  <h4><center>POS</center></h4>
     </a>
   </li>
   <li class="span2">
     <a href="<?php echo site_url("sales");?>" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/icons/sales&purchase.png")?> alt="sales&purchase">
-	  <h4><center>Sales&Purchase</center></h4>
+      <img src=<?php echo base_url("resources/images/icons/salesandpurchases.png")?> alt="sales/purchases"  width="90" height="90">
+	  <h4><center>Sales/Purchases</center></h4>
     </a>
   </li>  
 </ul>
@@ -36,27 +36,32 @@
 <ul class="thumbnails">
   <li class="span2">
     <a href="#" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/applogo.png")?> alt="Google+">
+      <img src=<?php echo base_url("resources/images/icons/googleplus.png")?> alt="Google+" width="90" height="90">
+	  <h4><center>Google+</center></h4>
     </a>
   </li>
   <li class="span2">
     <a href="#" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/applogo.png")?> alt="Facebook">
+      <img src=<?php echo base_url("resources/images/icons/newsletters.png")?> alt="newsletter"  width="90" height="90">
+	  <h4><center>Newsletter</center></h4>
     </a>
   </li>
   <li class="span2">
     <a href="#" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/applogo.png")?> alt="Twitter">
+      <img src=<?php echo base_url("resources/images/icons/twitter.png")?> alt="Twitter" width="90" height="90">
+	  <h4><center>Twitter</center></h4>
     </a>
   </li>
   <li class="span2">
     <a href="#" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/applogo.png")?> alt="Newsletter">
+      <img src=<?php echo base_url("resources/images/icons/facebook.png")?> alt="Facebook" width="90" height="90">
+	  <h4><center>Facebook</center></h4> 
     </a>
   </li>  
   <li class="span2">
     <a href="#" class="thumbnail">
-      <img src=<?php echo base_url("resources/images/applogo.png")?> alt="Google Analytics">
+      <img src=<?php echo base_url("resources/images/icons/googleanalytics.png")?> alt="Google Analytics" width="90" height="90">
+	  <h4><center>Google Analytics</center></h4> 
     </a>
   </li>   
 </ul> 
