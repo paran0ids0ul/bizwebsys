dfce5ec3df66d5e19c95b44f9e729b36469fa4aa
businesscloud <business_cloud@hotmail.com>
2013-03-08 09:52:21 +0000

    Add side menu

--- application/controllers/sales.php
+++ application/controllers/sales.php
@@ -2,6 +2,14 @@
 
 
 class Sales extends MY_Controller {
+	function __construct()
+	{	
+
+		parent::__construct();
+		//side menu
+		$this->data["sidemenu"] = $this->load->view("app/sales/sidemenu",'',true);
+	}
+
 	public function index(){	
 		$this->_render('app/sales/sales');
 	}
@@ -11,10 +19,14 @@ class Sales extends MY_Controller {
 	}
 	
 	public function display_order($orderID="SO0001",$customer="cust1",$date="7/03/2013"){
+
+		
+		//data
 		$this->data["orderID"] = $orderID;
 		$this->data["customer"] = $customer;
 		$this->data["date"] = $date;
-		$this->_render('app/sales/display_order',$orderID);
+		
+		$this->_render('app/sales/display_order');
 	}
 	
 }
