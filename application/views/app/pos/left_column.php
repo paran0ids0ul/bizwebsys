b3d7460706aa78167a62f220bd26ea3a02c39ca4
businesscloud <business_cloud@hotmail.com>
2013-03-09 13:44:01 +0000

    Fix left_column of POS

--- application/views/app/pos/left_column.php
+++ application/views/app/pos/left_column.php
@@ -1,4 +1,5 @@
-<div class="row myform-container">
+<div class="container">
+	<div class="row myform-container">
 		<!-- Shopping List -->
 		<div class="span4 myform">
 			<table class="table table-hover">
@@ -29,27 +30,28 @@
 					</div>						
 				</div>
 			</div>	
-	</div>	
-	<!-- Keypad Buttons -->
-	<div class="row keypad">
-		<div class="span4">
-			<div class="row">
-				<div class="span4">
-					<div class="span2">
-						<Button class="btn btn-large">Cash</Button>
+		</div>	
+		<!-- Keypad Buttons -->
+		<div class="row keypad">
+			<div class="span4">
+				<div class="row">
+					<div class="span4">
+						<div class="span2">
+							<Button class="btn btn-large">Cash</Button>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="span4">
+				<div class="row">
+					<div class="span4">
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="span4">
+				<div class="row">
+					<div class="span4">
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="span4">
+				<div class="row">
+					<div class="span4">
+					</div>
 				</div>
 			</div>
 		</div>
