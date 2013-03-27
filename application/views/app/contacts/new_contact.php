40856b68b82845d0b334df88a553ef3063576ae4
Cheung Tin Timothy Szeto <tins820@live.hk>
2013-03-27 21:38:20 +0000

    130327 2138
    UPDATE CONTACTS' VIEW

--- application/views/app/contacts/new_contact.php
+++ application/views/app/contacts/new_contact.php
@@ -26,18 +26,26 @@
 								</a>
 							</div>
 							<div class="span1 content">
+									
 									<label>First Name</label>									
 								</div>
-								<div class="span7 content">
-									<span class="input-xlarge uneditable-input" style="width:60%" id="contact_firstname">php to retrive data</span>
+								<div class="span3 content">
+									<span class="input-xlarge uneditable-input" style="width:90%" id="contact_firstname">php to retrive data</span>
 								</div>
 								
 								<div class="span1 content">
 									<label>Surname</label>									
 								</div>
-								<div class="span7 content">
-									<span class="input-xlarge uneditable-input" style="width:30%"id="contact_surname">php to retrive data</span>
-							</div>
+								<div class="span3 content">
+									<span class="input-xlarge uneditable-input" style="width:60%"id="contact_surname">php to retrive data</span>
+								</div>
+								<div class="span1 content">
+									<label>Common Name</label>									
+								</div>
+								<div class="span6 content">
+									<span class="input-xlarge uneditable-input" style="width:90%"id="contact_surname">php to retrive data</span>
+								</div>
+								
 									
 						
 					</div>  <!-- close upper contact-->
@@ -56,22 +64,29 @@
 									<input id="contact_country" type="text" placeholder="Country" >
 								</div>
 								
-								<label class="span1">Website	
-								</label>
-								<div class="span2 label-field">				
-									<input id="contact_website" type="text" placeholder="Website Address" >
-								</div>
-							
+															
 							</div> <!-- close left lower form-->
 						
 						
 						
 							<div class="span5">
 							
-								<label class="span1">Phone	
+								<label class="span1">Mobile	
+								</label>
+								<div class="span3 label-field">
+									<input id="mobile" type="text" placeholder="S97789789789" >
+								</div>
+								
+								<label class="span1">Telephone	
 								</label>
 								<div class="span3 label-field">
 									<input id="contact_phone" type="text" placeholder="S97789789789" >
+								</div>
+								
+								<label class="span1">Fax	
+								</label>
+								<div class="span3 label-field">
+									<input id="contact_fax" type="text" placeholder="S97789789789" >
 								</div>
 										
 								<label class="span1">E-mail	
@@ -80,29 +95,14 @@
 									<input id="contact_email" type="text" placeholder="123@123.com" >
 								</div>
 								
-								<label class="span1">Title
-								</label>
-								<div class="span3 label-field" id="title_select">
-									<select name="title_list">
-										<option>Please Choose :</option>
-										<option>Mr.</option>
-										<option>Ms.</option>
-										<option>Mrs.</option>
-									</select>						
-								</div>
+								
 								<label class="span1 is-business">Organization	
 								</label>
 								<div class="span3 label-field is-business">				
 									<input id="contact_organization" type="text" placeholder="Organization" >
 								</div>
 								
-								<label class="span1 is-business">Category	
-								</label>
-								<div class="span3 label-field is-business">				
-									<select name="contact_business" id="contact_business">
-									<option>Please Choose :</option>
-								</select>
-								</div>
+								
 								
 							</div> <!-- close right lower form -->
 							
